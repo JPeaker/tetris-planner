5b749d185043457e4a656a41340a0f50e0b2dc9b
Josh Peaker <3524665+JPeaker@users.noreply.github.com>
2020-07-11 16:57:10 +0100

    Updates

---
+++ precache-manifest.fe7dbafe0869392f3fbec0be914eca75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d7308ef8af190d9f8528b6ade1dddf",
+    "url": "/tetris-planner/index.html"
+  },
+  {
+    "revision": "a459990e500ae66472e9",
+    "url": "/tetris-planner/static/css/main.7e64ae93.chunk.css"
+  },
+  {
+    "revision": "e6dc10828033e0e22209",
+    "url": "/tetris-planner/static/js/2.ea6c09da.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/tetris-planner/static/js/2.ea6c09da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a459990e500ae66472e9",
+    "url": "/tetris-planner/static/js/main.0931e39a.chunk.js"
+  },
+  {
+    "revision": "0eedfcacf35035057884",
+    "url": "/tetris-planner/static/js/runtime-main.d4093a68.js"
+  }
+]);
